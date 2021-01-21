f7be24f7cddbde660c3d135aa859e72bbeb07858
NowakArtur97 <nowart97@gmail.com>
2021-01-21 07:54:07 +0100

    Generate project with Spring Initializr

---
+++ src/main/java/com/nowakArtur97/myMoments/myMoments/MyMomentsApplication.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.myMoments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMomentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMomentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowakArtur97/myMoments/myMoments/MyMomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.myMoments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
