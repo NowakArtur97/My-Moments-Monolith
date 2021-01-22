fdb5d7e101239ab6a6d419695bba0e96014eb85b
NowakArtur97 <nowart97@gmail.com>
2021-01-22 10:50:25 +0100

    Create Flyway Configuration

---
+++ src/main/java/com/nowakArtur97/myMoments/configuration/FlywayConfiguration.java
@@ -0,0 +1,26 @@
+package com.nowakArtur97.myMoments.configuration;
+
+import lombok.AllArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@AllArgsConstructor
+class FlywayConfiguration {
+
+    private final Environment env;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+
+        return new Flyway(
+                Flyway.configure()
+                        .baselineOnMigrate(false)
+                        .dataSource(
+                                env.getRequiredProperty("spring.flyway.url"),
+                                env.getRequiredProperty("spring.flyway.user"),
+                                env.getRequiredProperty("spring.flyway.password")));
+    }
+}
