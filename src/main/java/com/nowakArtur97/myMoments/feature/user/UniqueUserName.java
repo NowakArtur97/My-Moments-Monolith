20bd964977588c7775b2f5298e7eec880f95670d
NowakArtur97 <nowart97@gmail.com>
2021-02-01 10:27:57 +0100

    Create Unique User Name annotation

---
+++ src/main/java/com/nowakArtur97/myMoments/feature/user/UniqueUserName.java
@@ -0,0 +1,24 @@
+package com.nowakArtur97.myMoments.feature.user;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = UniqueUserNameConstraintValidator.class)
+@Target({FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface UniqueUserName {
+
+    String message() default "User name is already taken.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
