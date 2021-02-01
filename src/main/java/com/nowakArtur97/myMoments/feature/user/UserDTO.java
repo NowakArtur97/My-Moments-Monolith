a265077f8d5c5953be0792cb5432a9b71ff0c406
NowakArtur97 <nowart97@gmail.com>
2021-02-01 09:18:01 +0100

    Create User DTO

---
+++ src/main/java/com/nowakArtur97/myMoments/feature/user/UserDTO.java
@@ -0,0 +1,55 @@
+package com.nowakArtur97.myMoments.feature.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class UserDTO {
+
+    @NotBlank(message = "{user.name.notBlank}")
+    @Size(min = 5, max = 40, message = "{user.name.size}")
+    private String username;
+
+    @Email(message = "{user.email.wrongFormat}")
+    @NotBlank(message = "{user.email.notBlank}")
+    private String email;
+
+    @NotBlank(message = "{user.password.notBlank}")
+    private String password;
+
+    @NotBlank(message = "{user.matchingPassword.notBlank}")
+    private String matchingPassword;
+
+    @Valid
+    private UserProfileDTO profile;
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof UserDTO)) return false;
+
+        UserDTO userDTO = (UserDTO) o;
+
+        return Objects.equals(getUsername(), userDTO.getUsername()) &&
+                Objects.equals(getEmail(), userDTO.getEmail()) &&
+                Objects.equals(getPassword(), userDTO.getPassword()) &&
+                Objects.equals(getMatchingPassword(), userDTO.getMatchingPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getEmail(), getPassword(), getMatchingPassword());
+    }
+}
--- src/main/java/com/nowakArtur97/myMoments/feature/user/UserProfileDTO.java
+++ src/main/java/com/nowakArtur97/myMoments/feature/user/UserProfileDTO.java
@@ -15,19 +15,19 @@ import javax.validation.constraints.Size;
 @Builder
 class UserProfileDTO {
 
-    @Size(message = "{userProfile.about.size}{max}", max = 250)
+    @Size(message = "{userProfile.about.size}", max = 250)
     private String about;
 
-    @NotNull(message = "{userProfile.gender.size}{max}")
+    @NotNull(message = "{userProfile.gender.notNull}")
     private Gender gender;
 
-    @Size(message = "{userProfile.interests.size}{max}", max = 250)
+    @Size(message = "{userProfile.interests.size}", max = 250)
     private String interests;
 
-    @Size(message = "{userProfile.languages.size}{max}", max = 250)
+    @Size(message = "{userProfile.languages.size}", max = 250)
     private String languages;
 
-    @Size(message = "{userProfile.location.size}{max}", max = 50)
+    @Size(message = "{userProfile.location.size}", max = 50)
     private String location;
 
     private MultipartFile image;
