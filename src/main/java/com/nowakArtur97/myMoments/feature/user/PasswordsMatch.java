aed0733a5ce1eac37dc58288c0e36c9c3da01105
NowakArtur97 <nowart97@gmail.com>
2021-02-01 09:26:03 +0100

    Create Passwords Match Constraint Validator

--- src/main/java/com/nowakArtur97/myMoments/feature/user/PasswordsMatch.java
+++ src/main/java/com/nowakArtur97/myMoments/feature/user/PasswordsMatch.java
@@ -11,7 +11,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy =  PasswordsMatchConstraintValidator.class)
+@Constraint(validatedBy = PasswordsMatchConstraintValidator.class)
 @Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 public @interface PasswordsMatch {
