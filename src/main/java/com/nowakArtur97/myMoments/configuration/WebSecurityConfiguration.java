e5efa451a5a2d935bca5125b0d8ef64057f10c75
NowakArtur97 <nowart97@gmail.com>
2021-02-02 08:12:51 +0100

    Setup Authentication Provider in Web Security Configuration

--- src/main/java/com/nowakArtur97/myMoments/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/nowakArtur97/myMoments/configuration/WebSecurityConfiguration.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +25,17 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     PasswordEncoder getBCryptPasswordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider getDaoAuthenticationProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(getBCryptPasswordEncoder());
+
+        return daoAuthenticationProvider;
     }
 
     @Bean
