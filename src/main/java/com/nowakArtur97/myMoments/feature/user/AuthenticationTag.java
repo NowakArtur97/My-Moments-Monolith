018ba9a2e3098282cf88aacde3d460711c153268
NowakArtur97 <nowart97@gmail.com>
2021-02-02 09:24:09 +0100

    Create Authentication Tag

---
+++ src/main/java/com/nowakArtur97/myMoments/feature/user/AuthenticationTag.java
@@ -0,0 +1,8 @@
+package com.nowakArtur97.myMoments.feature.user;
+
+public final class AuthenticationTag {
+
+    public static final String RESOURCE = "User Authentication";
+
+    public static final String DESCRIPTION = "Operations related to User Authentication";
+}
