b8ab39e30ba1d7cc0cd513aba3baf760506e8f93
NowakArtur97 <nowart97@gmail.com>
2021-02-03 07:50:47 +0100

    Fix issue with returnig Authentication Response in Authentication Controller

--- src/main/java/com/nowakArtur97/myMoments/feature/user/AuthenticationResponse.java
+++ src/main/java/com/nowakArtur97/myMoments/feature/user/AuthenticationResponse.java
@@ -3,14 +3,16 @@ package com.nowakArtur97.myMoments.feature.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-@ApiModel(description = "API key")
+@Getter
 @AllArgsConstructor
+@ApiModel(description = "API key")
 class AuthenticationResponse {
 
     @ApiModelProperty(notes = "Generated token")
-    private final String token;
+    private String token;
 
     @ApiModelProperty(notes = "Expiration time in milliseconds")
-    private final long expirationTimeInMilliseconds;
+    private long expirationTimeInMilliseconds;
 }
