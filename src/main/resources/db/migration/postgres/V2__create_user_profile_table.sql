cbb894d7ef5a07a92368ef54f342bfb1a7072cd6
NowakArtur97 <nowart97@gmail.com>
2021-08-10 17:46:52 +0200

    Create migration script for PostgreSQL for inserting default users and roles and fix issues with table references

--- src/main/resources/db/migration/postgres/V2__create_user_profile_table.sql
+++ src/main/resources/db/migration/postgres/V2__create_user_profile_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE my_moments.user_profile (
     "id" SERIAL PRIMARY KEY,
+    "uuid" VARCHAR(36) NOT NULL,
     "about" TEXT DEFAULT '',
     "gender" TEXT NOT NULL,
     "interests" TEXT DEFAULT '',
@@ -10,5 +11,5 @@ CREATE TABLE my_moments.user_profile (
     "modify_date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "version" INT DEFAULT 0 NOT NULL,
 
-    CONSTRAINT "FK_USER_USER_PROFILE" FOREIGN KEY ("id") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "FK_USER_USER_PROFILE" FOREIGN KEY ("id") REFERENCES my_moments.user ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
